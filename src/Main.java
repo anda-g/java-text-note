98031055dd9876651c57aabf98c81ec10194bf37
KungSovannda <kungsovannda@gmail.com>
2025-05-10 16:21:13 +0700

    feat: flatlaf lookandfeel

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,17 @@
+import com.formdev.flatlaf.FlatLightLaf;
+import com.formdev.flatlaf.themes.FlatMacLightLaf;
+
+import javax.swing.*;
+
 public class Main{
     public static void main(String[] args) {
+
+        try{
+            UIManager.setLookAndFeel(new FlatMacLightLaf());
+        } catch (UnsupportedLookAndFeelException e) {
+            throw new RuntimeException(e);
+        }
+
         new TextEditor();
     }
 }
